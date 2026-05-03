6e96a881dc9dd82a5e68cb95ad5813147e29c2ae
saif9657 <sams.ahamed12@gmail.com>
2026-05-03 17:31:12 +0530

    initial backend setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Start server
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
